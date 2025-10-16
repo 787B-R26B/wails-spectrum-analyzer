32c4fb495531ce7b4d849a216f3087e8d43d9c43
787B-R26B <s.tomoyasu.sa22c@gmail.com>
2025-10-16 09:17:03 +0900

    feat: reset EQ with doubble click or right click

--- frontend/src/components/ui/Equalizer.tsx
+++ frontend/src/components/ui/Equalizer.tsx
@@ -56,6 +56,11 @@ export function Equalizer({
             step="0.05"
             value={preGain}
             onChange={(e) => onPreGainChange(parseFloat(e.target.value))}
+            onDoubleClick={() => onPreGainChange(1)}
+            onContextMenu={(e) => {
+              e.preventDefault();
+              onPreGainChange(1);
+            }}
             className="w-5 h-32 appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-black/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-green-500"
             style={{ writingMode: "vertical-lr", direction: "rtl" }}
           />
@@ -75,6 +80,11 @@ export function Equalizer({
               step="0.5"
               value={gains[i]}
               onChange={(e) => onGainChange(i, parseFloat(e.target.value))}
+              onDoubleClick={() => onGainChange(i, 0)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                onGainChange(i, 0);
+              }}
               className="w-5 h-32 appearance-none bg-transparent [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:bg-black/25 [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-blue-500"
               style={{ writingMode: "vertical-lr", direction: "rtl" }}
             />
